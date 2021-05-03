ccfd2e71e4f1f5d5ca4a1def5f8b6e449abbb2bf
Stanly0407 <Shelestova_S_V@mail.ru>
2021-05-03 13:56:16 +0300

    remember page logic

--- src/main/java/com/epam/web/Controller.java
+++ src/main/java/com/epam/web/Controller.java
@@ -12,11 +12,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Controller extends HttpServlet {
     private static final Logger LOGGER = LogManager.getLogger(Controller.class);
     private static final String PARAMETER_COMMAND = "command";
+    private static final String CURRENT_PAGE = "currentPage";
+    private static final String START_PAGE = "/";
 
     private CommandFactory commandFactory = new CommandFactory();
 
@@ -30,16 +33,22 @@ public class Controller extends HttpServlet {
         process(request, response);
     }
 
-
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        String currentPage = (String) session.getAttribute(CURRENT_PAGE);
+        LOGGER.debug("session CURRENT_PAGE" + currentPage);
+        if (currentPage == null) {
+            session.setAttribute(CURRENT_PAGE, START_PAGE);
+        }
+
         String commandType = request.getParameter(PARAMETER_COMMAND);
-        LOGGER.debug("PARAMETER_COMMAND = " + commandType);
         Command command = commandFactory.create(commandType);
         String page = null;
         boolean isRedirect = false;
         try {
             CommandResult result = command.execute(request, response);
             page = result.getPage();
+            LOGGER.debug("commandType = " + commandType);
             isRedirect = result.isRedirect();
         } catch (ServiceException e) {
             request.setAttribute("errorMessage ----", e.getMessage());
@@ -51,8 +60,14 @@ public class Controller extends HttpServlet {
         if (!isRedirect) {
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(page);
             requestDispatcher.forward(request, response);
+            if (!commandType.equals("changeLanguage")) {
+                session.setAttribute(CURRENT_PAGE, ("/controller?command=" + commandType));
+            }
+            LOGGER.debug("forward_ " + page);
         } else {
             response.sendRedirect(request.getContextPath() + page);
+            session.setAttribute(CURRENT_PAGE, page);
+            LOGGER.debug("redirect_" + page);
         }
     }
 
